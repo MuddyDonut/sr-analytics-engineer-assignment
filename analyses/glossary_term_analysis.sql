66f0097433c2ae06fe1cda6a801f327cf101509f
Julian <julian1559@gmail.com>
2025-01-19 00:27:32 -0500

    suggestion/sql-refactor

--- analyses/glossary_term_analysis.sql
+++ analyses/glossary_term_analysis.sql
@@ -9,27 +9,32 @@ Questions to Answer:
 
 with urns_with_terms as (
 select
-    urn,
-    glossary_terms,
-    json_extract_string(term.value, '$.urn') as term_urn
+    urn
+    , glossary_terms
+    , json_extract_string(term.value, '$.urn') as term_urn
 from
     stg_datahub_entities
-cross join unnest(json_extract(glossary_terms, '$.terms')::json[]) as term(value)
+    , unnest(json_extract(glossary_terms, '$.terms')::json[]) as term(value)
 where
     glossary_terms is not null
+),
+
+term_details as (
+    select 
+      json_extract_string(stg_datahub_entities.entity_details, '$.name') as term_name
+      , urn  
+    from stg_datahub_entities
 )
 
 select
-  -- stg_datahub_entities.urn as term_urn,
-  json_extract_string(stg_datahub_entities.entity_details, '$.name') as term_name
-  , count(distinct urns_with_terms.urn) as urn_count
+    term_name
+    , count(*) as urn_count
 from
-  urns_with_terms
-left join
-  stg_datahub_entities
-  on stg_datahub_entities.urn = urns_with_terms.term_urn
-group by 1--, 2
-order by 2 desc
+    term_details
+join
+    urns_with_terms on term_details.urn = term_urn
+group by term_name
+order by count(*) desc
 ;
 
 /*
